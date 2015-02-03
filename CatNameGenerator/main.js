98c48a7ddf49e84320f830c2a53181e1d9e7448b
Tobias Johansson <T_johansson92@hotmail.com>
2015-02-03 14:00:50 +0100

    updated cat names

--- CatNameGenerator/main.js
+++ CatNameGenerator/main.js
@@ -1,36 +1,36 @@
-
-$(document).ready(function(){
+$(document).ready(function() {
 
     //Button on click function
     $(".btn").on("click", getCatName);
-    
-    
+
+
     //Create array of catnames and get random
-    
+
     //just so the browser doesn't have to find span every time we press button
     var $write = $("h3");
 
-    function getCatName(){
-        
+    function getCatName() {
+
         var catArray = ["Simba",
-        	"Kalle", "Torsten", "Kongo", "Drogo", "Hankey",
-         	"Dovah", "Lex", "Tess", "Sizzy", "Lurch", "Damp", "Benjamin",
-         	"Godzilla", "Charlie", "Minto", "Claws", "Mario", "Ronaldo",
-         	"Zlatan", "Goofer", "Rocky", "Bilbo", "Ursula", "Jennifer",
-         	"Gringo", "Generalen", "Cellsor", "Gara", "Steve", "Magneto"];
+            "Kalle", "Salty", "Kongo", "Drogo", "Kitty",
+            "Dovah", "Lex", "Tess", "Tigger", "Lurch", "Loki", "Coco",
+            "Godzilla", "Charlie", "Minto", "Dexter", "Mario", "Ronaldo",
+            "Zlatan", "Jacky", "Rocky", "Thor", "Bella", "Molly",
+            "Gringo", "Generalen", "Snickers", "Max", "Steve", "Magneto"
+        ];
 
         //random value between 0-array length
         var randomCat = Math.floor(Math.random() * catArray.length);
-        
+
         var i = 0;
 
         //This resets button every time u click it
-        while(i < 1){
-        	$write.html("You should name your cat <span>"+catArray[randomCat]+"</span>");
-        	i++;
-        }  
+        while (i < 1) {
+            $write.html("You should name your cat <span>" + catArray[randomCat] + "</span>");
+            i++;
+        }
 
-           
+
     }
 
 });
